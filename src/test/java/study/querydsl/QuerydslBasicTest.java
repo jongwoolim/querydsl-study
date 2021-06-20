e67cd104d776a35f445260e36d4e8b2182e931c8
limjongwoo <whddn528@naver.com>
2021-06-20 14:43:19 +0900

    JPQL & querydsl test add

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,81 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory jpaQueryFactory;
+
+    @BeforeEach
+    public void before(){
+        jpaQueryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+    }
+
+    @Test
+    public void startJPQL(){
+        //member1 찾기
+        final String qlString = "select m from Member m where m.username = :username";
+        final Member findByJPQL = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findByJPQL.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startquerydsl(){
+
+        jpaQueryFactory = new JPAQueryFactory(em);
+        //QMember m = new QMember("m");
+        //final QMember m = QMember.member;
+
+//        final Member findMember = jpaQueryFactory
+//                .select(m)
+//                .from(m)
+//                .where(m.username.eq("member1"))
+//                .fetchOne();
+        final Member findMember = jpaQueryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+}
