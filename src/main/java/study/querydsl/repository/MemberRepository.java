f81fd89a3861f545d0819aaa3e1a754c0a539414
limjongwoo <whddn528@naver.com>
2021-06-27 20:44:57 +0900

    QuerydslPredicateExecutor interface

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+// QuerydslPredicateExecutor 조인 x, 클라이언트가 querydsl에 의존... 따라서 복잡한 실무에서는 권장x
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 }
