7676a90f63ec4321e614c444d77cd44aa77bd875
limjongwoo <whddn528@naver.com>
2021-06-27 20:13:22 +0900

    countquery optimization

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,10 +2,12 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -98,7 +100,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 .fetch();// 컨텐츠용 쿼리, 카운터 쿼리 두번 날린다
 
         //카운터 쿼리 최적화 - 별도의 쿼리 작성 불필요한 조인 제거.. ex)데이터가 많은 경우
-        final long total = queryFactory
+        final JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -107,9 +109,14 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                ).fetchCount();
+                );
 
-        return new PageImpl<>(content, pageable, total);
+        //카운터 쿼리 최적화
+        //count 쿼리 생략 가능한 경우 생략해서 처리한다
+        // 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 적을 때
+        // 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
+//        return new PageImpl<>(content, pageable, total);
 
     }
 
